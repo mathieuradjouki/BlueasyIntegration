91ad574fc9202de4191d1473d38f9cf4cf4324a6
mathieuradjouki <m.radjouki@codeur.online>
2020-05-30 17:35:21 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="main.css" rel="stylesheet" media="all" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <title>Blueasy integration</title>
+  </head>
+    <body>
+      <div class="container-fluid p-0">
+        <div class="header">
+        <!--nav-->
+          <nav class="navbar navbar-expand-sm nav-color">
+            <!-- BLUEASY/logo -->
+              <div class="container font-weight-bold">
+                <a class="navbar-brand text-white nav-logo" href="#">BLU<span class="text-info">E</span>ASY</a>
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link text-white" href="#">SERVICES</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white" href="#">SERVICES</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white" href="#">FEATURE</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white" href="#">PORTFOLIO</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white" href="#">CONTACT</a>
+                    </li>
+                  </ul>
+              </div>
+          </nav>
+              <div class="container">
+                <h1 class="text-light d-flex align-items-center flex-column justify-content-center h-100">
+                  <p>"I'M LOOKING FOR THE UNEXPECTED"</p>
+                  <p>"I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE"</p>
+                </h1>
+              </div>
+        </div>
+        <!--SERVICES-->
+        <section class="services py-5">
+            <div class="container">
+              <h2 class="title-services">SERVICES</h2>
+              <img src="image/line-compressor" alt="line" class="line-services">
+            <div class="row">
+                <div class="card-deck">
+                  <div class="card services-card col-lg-3">
+                    <img src="image/coffee-logo-compressor.png" class="card-img-top coffee-logo" alt="image-coffee">
+                    <div class="card-body  p-0">
+                      <h5 class="card-title font-weight-bold my-3">COFFEE</h5>
+                      <p class="card-text">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                    </div>
+                  </div>
+                  <div class="card services-card">
+                    <img src="image/instan-logo-compressor" class="card-img-top instan-logo" alt="image-instan">
+                    <div class="card-body p-0">
+                      <h5 class="card-title font-weight-bold my-3">INSTANT</h5>
+                      <p class="card-text">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                    </div>
+                  </div>
+                  <div class="card services-card">
+                    <img src="image/serious-logo-compressor" class="card-img-top serious-logo" alt="image-logo">
+                    <div class="card-body p-0">
+                      <h5 class="card-title font-weight-bold my-3">SERIOUS</h5>
+                      <p class="card-text">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                    </div>
+                  </div>
+                  <div class="card services-card">
+                    <img src="image/logo4-compressor" class="card-img-top frame-logo" alt="frame-logo">
+                    <div class="card-body p-0">
+                      <h5 class="card-title font-weight-bold my-3">FRAME</h5>
+                      <p class="card-text">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+      </section>
+      <!--PORTFOLIO-->
+      <section class="portfolio py-5">
+        <div class="container">
+          <div class="row">
+          <h2 class="title-portfolio">PORTFOLIO</h2>
+
+          <ul class="nav nav-portfolio mr-0 ml-auto">
+            <li class="nav-item">
+              <a class="nav-link portfolio-link" href="#">ALL</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link portfolio-link" href="#">PANORAMAS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link portfolio-link" href="#">PORTRAITS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link portfolio-link" href="#">MACRO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link portfolio-link" href="#">JOURNAL</a>
+            </li>
+          </ul>
+        </div>
+        <img src="image/line-portfolio-compressor" alt="line" class="line-portfolio ml-n3">
+        <div class="row portfolio-card  row-cols-sm-2 row-cols-md-4 my-4">
+          <div class="col pl-0 pr-2">
+            <img src="image/card-1-compressor.jpg" alt="photo">
+          </div>
+          <div class="col pl-0 pr-2">
+            <img src="image/card-2-compressor.jpg" alt="photo">
+          </div>
+          <div class="col pl-0 pr-2">
+            <img src="image/card-3-compressor.jpg" alt="photo">
+          </div>
+          <div class="col pl-0 pr-2">
+            <img src="image/card-4-compressor.jpg" alt="photo">
+          </div>
+        </div>
+        <div class="row portfolio-card  row-cols-sm-2 row-cols-md-4">
+          <div class="col pl-0 pr-2">
+            <img src="image/card-5-compressor.jpg" alt="photo">
+          </div>
+          <div class="col pl-0 pr-2">
+            <img src="image/card-6-compressor.jpg" alt="photo">
+          </div>
+          <div class="col pl-0 pr-2">
+            <img src="image/card-7-compressor.jpg" alt="photo">
+          </div>
+          <div class="col pl-0 pr-2">
+            <img src="image/card-8-compressor.jpg" alt="photo">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--JUST DEFAULT SECTION-->
+    <section class="just-section">
+      <div class="container px-0 py-5">
+        <div class="row cols-lg-12 cols-md-8 cols-sm-6 px-0">
+          <div class="col col-just-text">
+            <h2>JUST DEFAULT SECTION</h2>
+            <img src="image/line-compressor" alt="line" class="line-services">
+            <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheescake bownie</p>
+            <p>Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+            <button type="button" class="btn btn-outline-secondary rounded-0 font-weight-bold">VISIT ME</button>
+          </div>
+          <div class="col col-just-text">
+            <iframe class="responsive-iframe" src="https://www.youtube.com/embed/5eO9xc8aABY"></iframe>
+          </div>
+      </div>
+    </section>
+    <!--TWITTER-->
+    <section class="twitter">
+        <div class="container px-o py-5">
+          <div class="row">
+            <div class="col">
+              <h2 class="twitter-title">TWITTER</h2>
+            </div>
+            <div class="col justify-content-end">
+              <i class="fab fa-twitter"></i>
+            </div>
+          </div>
+            <img src="image/line-compressor" alt="line" class="line-services">
+            <p>Underwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake</p>
+            <p>@cheesecake tootsie roll.</p>
+      </div>
+    </section>
