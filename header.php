2ceb18fb0d809fdcca61eeb4ae5d0e319e9c76c1
mathieuradjouki <m.radjouki@codeur.online>
2020-06-03 11:57:21 +0200

    modificaton

--- header.php
+++ header.php
@@ -9,37 +9,40 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="main.css" rel="stylesheet" media="all" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.">
     <title>Blueasy integration</title>
   </head>
     <body>
       <div class="container-fluid p-0">
         <div class="header">
         <!--nav-->
-          <nav class="navbar navbar-expand-sm nav-color">
+          <nav class="navbar navbar-expand-sm nav-color p-5">
             <!-- BLUEASY/logo -->
-              <div class="container font-weight-bold">
-                <a class="navbar-brand text-white nav-logo" href="#">BLU<span class="text-info">E</span>ASY</a>
+              <div class="container font-weight-bold px-0">
+                <a class="navbar-brand nav-logo" href="#">BLU<span class="text-info">E</span>ASY</a>
                   <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="#">SERVICES</a>
+                      <a class="nav-link" href="#">HOME</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="#">SERVICES</a>
+                      <a class="nav-link" href="#">SERVICES</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="#">FEATURE</a>
+                      <a class="nav-link" href="#">FEATURE</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="#">PORTFOLIO</a>
+                      <a class="nav-link" href="#">PORTFOLIO</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="#">CONTACT</a>
+                      <a class="nav-link" href="#">CONTACT</a>
                     </li>
                   </ul>
               </div>
           </nav>
-              <div class="container">
-                <h1 class="text-light d-flex align-items-center flex-column justify-content-center h-100">
+              <div class="container px-0">
+                <h1 class="text-light d-flex align-items-center flex-column justify-content-center">
                   <p>"I'M LOOKING FOR THE UNEXPECTED"</p>
                   <p>"I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE"</p>
                 </h1>
@@ -47,12 +50,12 @@
         </div>
         <!--SERVICES-->
         <section class="services py-5">
-            <div class="container">
+            <div class="container px-0">
               <h2 class="title-services">SERVICES</h2>
-              <img src="image/line-compressor" alt="line" class="line-services">
-            <div class="row">
-                <div class="card-deck">
-                  <div class="card services-card col-lg-3">
+              <img src="image/line-compressor" alt="line" class="line-services mt-n3">
+            <div class="row mt-3 mw-100 ml-2">
+                <div class="card-deck mw-100 pl-2">
+                  <div class="card services-card col-lg-3 p-0 mx-0">
                     <img src="image/coffee-logo-compressor.png" class="card-img-top coffee-logo" alt="image-coffee">
                     <div class="card-body  p-0">
                       <h5 class="card-title font-weight-bold my-3">COFFEE</h5>
@@ -86,55 +89,78 @@
       </section>
       <!--PORTFOLIO-->
       <section class="portfolio py-5">
-        <div class="container">
-          <div class="row">
-          <h2 class="title-portfolio">PORTFOLIO</h2>
-
-          <ul class="nav nav-portfolio mr-0 ml-auto">
-            <li class="nav-item">
-              <a class="nav-link portfolio-link" href="#">ALL</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link portfolio-link" href="#">PANORAMAS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link portfolio-link" href="#">PORTRAITS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link portfolio-link" href="#">MACRO</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link portfolio-link" href="#">JOURNAL</a>
-            </li>
-          </ul>
+        <div class="container px-0">
+          <div class="row mx-0">
+            <h2 class="title-portfolio">PORTFOLIO</h2>
+              <ul class="nav nav-portfolio mr-0 ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link portfolio-link" href="#">ALL</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link portfolio-link" href="#">PANORAMAS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link portfolio-link" href="#">PORTRAITS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link portfolio-link" href="#">MACRO</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link portfolio-link" href="#">JOURNAL</a>
+                </li>
+              </ul>
+          </div>
+        <img src="image/line-portfolio-compressor" alt="line" class="line-portfolio">
+        <div class="row portfolio-card  row-cols-md-2 row-cols-lg-4 my-4 mx-0">
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-1-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
+          </div>
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-2-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
+          </div>
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-3-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
+          </div>
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-4-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
+          </div>
         </div>
-        <img src="image/line-portfolio-compressor" alt="line" class="line-portfolio ml-n3">
-        <div class="row portfolio-card  row-cols-sm-2 row-cols-md-4 my-4">
-          <div class="col pl-0 pr-2">
-            <img src="image/card-1-compressor.jpg" alt="photo">
+        <div class="row portfolio-card  row-cols-md-2 row-cols-lg-4 mx-0">
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-5-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
           </div>
-          <div class="col pl-0 pr-2">
-            <img src="image/card-2-compressor.jpg" alt="photo">
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-6-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
           </div>
-          <div class="col pl-0 pr-2">
-            <img src="image/card-3-compressor.jpg" alt="photo">
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-7-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
           </div>
-          <div class="col pl-0 pr-2">
-            <img src="image/card-4-compressor.jpg" alt="photo">
-          </div>
-        </div>
-        <div class="row portfolio-card  row-cols-sm-2 row-cols-md-4">
-          <div class="col pl-0 pr-2">
-            <img src="image/card-5-compressor.jpg" alt="photo">
-          </div>
-          <div class="col pl-0 pr-2">
-            <img src="image/card-6-compressor.jpg" alt="photo">
-          </div>
-          <div class="col pl-0 pr-2">
-            <img src="image/card-7-compressor.jpg" alt="photo">
-          </div>
-          <div class="col pl-0 pr-2">
-            <img src="image/card-8-compressor.jpg" alt="photo">
+          <div class="col pl-0 pr-2 photo">
+            <img src="image/card-8-compressor.jpg" alt="photo" class="img-portfolio">
+              <div class="overlay justify-content-center align-items-center d-flex">
+                <a href="#" class="text-white plus">+</a>
+              </div>
           </div>
         </div>
       </div>
@@ -143,7 +169,7 @@
     <section class="just-section">
       <div class="container px-0 py-5">
         <div class="row cols-lg-12 cols-md-8 cols-sm-6 px-0">
-          <div class="col col-just-text">
+          <div class="col cols-lg-5 col-just-text">
             <h2>JUST DEFAULT SECTION</h2>
             <img src="image/line-compressor" alt="line" class="line-services">
             <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheescake bownie</p>
@@ -151,8 +177,9 @@
             <button type="button" class="btn btn-outline-secondary rounded-0 font-weight-bold">VISIT ME</button>
           </div>
           <div class="col col-just-text">
-            <iframe class="responsive-iframe" src="https://www.youtube.com/embed/5eO9xc8aABY"></iframe>
+            <iframe src="https://player.vimeo.com/video/139826398" frameborder="0" allow="autoplay; fullscreen" allowfullscreen "mw-auto" "mh-auto" class="video"></iframe>
           </div>
+        </div>
       </div>
     </section>
     <!--TWITTER-->
@@ -171,3 +198,105 @@
             <p>@cheesecake tootsie roll.</p>
       </div>
     </section>
+    <!--JOHN DOE-->
+    <section class="john-doe">
+      <div class="container px-0 py-5">
+        <div class="row">
+          <div class="col">
+            <h2 class="twitter-title">WHO IS JOHN DOE?</h2>
+          </div>
+        </div>
+          <img src="image/line-compressor" alt="line" class="line-services">
+        <div class="row cols-lg-12 cols-md-8 cols-sm-6 px-0">
+          <div class="col col-just-text">
+            <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheescake bownie</p>
+            <p>Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+          </div>
+          <div class="col col-just-text">
+            <p>Pudding dessert jujubes gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee bear claw pastry.</p>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-google-plus-g"></i>
+            <i class="fas fa-basketball-ball"></i>
+            <i class="fas fa-coffee"></i>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--TESTIMONIALS-->
+    <section class="testomonials">
+      <div class="container px-0 py-5">
+        <div class="row">
+          <div class="col">
+            <h2 class="twitter-title">TESTIMONIALS</h2>
+          </div>
+          <div class="col justify-content-end">
+            <i class="fab fa-twitter"></i>
+          </div>
+        </div>
+          <img src="image/line-portfolio-compressor" alt="line" class="line-portfolio">
+          <p class="p-testomonials">Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</p>
+          <p class="walter-white text-info">-Walter White</p>
+      </div>
+    </section>
+    <!--CONTACT-->
+    <section class="contact">
+      <div class="container px-0 py-5">
+        <div class="row">
+          <div class="col">
+            <h2 class="twitter-title">CONTACT</h2>
+          </div>
+        </div>
+          <img src="image/line-compressor" alt="line" class="line-services">
+        <div class="row cols-lg-12 cols-md-8 cols-sm-6 px-0">
+          <div class="col col-just-text">
+            <div class="form-group">
+              <label for="usr">FULL NAME</label>
+              <input type="text" class="form-control background-transparent" id="usr">
+            </div>
+            <div class="form-group">
+              <label for="usr">EMAIL</label>
+              <input type="EMAIL" class="form-control background-transparent" id="email">
+            </div>
+          </div>
+          <div class="col col-just-text">
+            <p>Jujubes bear claw croissant wafer sweet lemon brownie chocolate bar. Sweet lollipop havah tootsie roll cake fruitcake. Chocolate cake chocolate bar gingerbread lollipop tart. Jujubes bear claw donut chupa chups.</p>
+            <p>Linda Newman<br>W325 State Road 123<br>Mondovi, WI(Winsconsin) 98746-54321<br>(715) 946 - 1234</p>
+          </div>
+        </div>
+        <div class="row cols-lg-12">
+          <div class="col">
+            <div class="form-group">
+              <label for="comment">MESSAGE</label>
+              <textarea class="form-control background-transparent" rows="5" id="comment"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="row col-lg-3">
+          <div class="col px-0">
+            <button type="button" class="btn btn-primary btn-lg btn-block">MESSAGE</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--FOOTER-->
+    <section class="footer">
+        <nav class="navbar navbar-expand-sm nav-color">
+            <div class="container px-0 py-5">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">HOME |</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">SERVICES |</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">PORTFOLIO |</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                  </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+  </div>
